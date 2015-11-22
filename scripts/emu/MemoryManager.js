e4173f4e0fdb0f2d84250c17146c23f07fe80120
Jovan Cejovic <jovan.cejovic@sbgenomics.com>
2015-11-22 02:59:49 +0100

    Add memory interface stub

---
+++ scripts/emu/MemoryManager.js
@@ -0,0 +1,27 @@
+/**
+ * Created by JovanCe on 11/22/15.
+ */
+
+
+define(["underscore", "CPU"], function(_, CPU) {
+    function MemoryManager() {
+    }
+
+    MemoryManager.prototype.readByte = function(addr) {
+
+    };
+
+    MemoryManager.prototype.writeByte = function(addr) {
+
+    };
+
+    MemoryManager.prototype.readWord = function(addr) {
+
+    };
+
+    MemoryManager.prototype.writeWord = function(addr) {
+
+    };
+
+    return MemoryManager;
+});
