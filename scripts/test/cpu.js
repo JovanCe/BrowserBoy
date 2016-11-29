e18edf72d2a517f0f18fcf13c017bc3647d445c4
Jovan Cejovic <jovan.cejovic@gmail.com>
2016-11-29 22:52:39 +0100

    Add basic cpu test

--- scripts/test/cpu.js
+++ scripts/test/cpu.js
@@ -4,13 +4,9 @@
 
 define(["CPU"], function(CPU) {
     describe("Sample Module", function() {
-        it('should have a name', function() {
-            expect(CPU.name).to.be.a("string");
-            expect(CPU.name).to.equal("CPU");
-        });
 
         it('should have its own dependencies', function() {
-            expect(CPU.dependency).to.equal("GPU");
+            expect(CPU._stop).to.equal(true);
         });
 
     });
