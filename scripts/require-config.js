130b1a97a0d121def8d9f0a94ecad4f206399e93
Jovan Cejovic <jovan.cejovic@gmail.com>
2016-11-29 22:47:51 +0100

    Add jquery (stupid mocha requires it -.-)

--- scripts/require-config.js
+++ scripts/require-config.js
@@ -8,6 +8,7 @@ requirejs.config({
         CPU: "emu/CPU",
         GPU: "emu/GPU",
         MemoryManager: "emu/MemoryManager",
-        lodash: "lib/lodash"
+        lodash: "lib/lodash",
+        jquery: "lib/jquery-3.1.1.min"
     }
 });
