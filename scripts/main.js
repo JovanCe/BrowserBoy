08bb45d2cf1a80de462bba48e6e17f4873080fdb
Jovan Cejovic <jovan.cejovic@sbgenomics.com>
2016-11-29 01:31:37 +0100

    Extract requirejs config to separate file

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,13 @@
  * Created by JovanCe on 11/8/15.
  */
 
-require.config({
-    baseUrl: "scripts",
-    paths: {
-        CPU: "emu/CPU",
-        GPU: "emu/GPU",
-        MemoryManager: "emu/MemoryManager",
-        lodash: "lib/lodash"
-    }
+require(["require-config"], function(){
+    require(["CPU", "MemoryManager", "GPU"],
+        function(CPU, MM, GPU) {
+            CPU.reset();
+            MM.reset();
+            MM.embedBios();
+            GPU.reset();
+            CPU.dispatch();
+        });
 });
-
-require(["CPU", "MemoryManager", "GPU"],
-    function(CPU, MM, GPU) {
-        CPU.reset();
-        MM.reset();
-        MM.embedBios();
-        GPU.reset();
-        CPU.dispatch();
-    });
---
+++ scripts/require-config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by JovanCe on 11/29/16.
+ */
+
+requirejs.config({
+    baseUrl: "scripts",
+    paths: {
+        CPU: "emu/CPU",
+        GPU: "emu/GPU",
+        MemoryManager: "emu/MemoryManager",
+        lodash: "lib/lodash"
+    }
+});
