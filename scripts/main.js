0d40ec3b39e4ef47441bbc01b0607b9898b770d5
Jovan Cejovic <jovan.cejovic@gmail.com>
2016-12-03 17:27:59 +0100

    Add rom testing page

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,16 @@
 require(["require-config"], function(){
     require(["CPU", "MemoryManager", "GPU"],
         function(CPU, MM, GPU) {
+            var fileInput = document.getElementById('rom');
+            fileInput.addEventListener("change", function(e) {
+                var file = fileInput.files[0];
+                MM.loadROM(file);
+
+            }, false);
+
             CPU.reset();
             MM.reset();
-            MM.embedBios();
+            //MM.embedBios();
             GPU.reset();
-            CPU.dispatch();
         });
 });
